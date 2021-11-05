7835bfc5cbb4387ae38398e58579077f6b4ceefc
Diego Vazquez <diego.vazquez@valoraapp.com>
2021-11-05 19:37:14 -0300

    CR Changes

--- src/database/migrations/20211026201444_create_historical_prices_table.ts
+++ src/database/migrations/20211026201444_create_historical_prices_table.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('historical_token_prices', (table) => {
     table.increments('id').primary()
     table.string('token').notNullable()
-    table.string('comparedToken').notNullable()
+    table.string('base_token').notNullable()
     table.dateTime('at').notNullable()
     // Using string to convert to big number
     table.string('price').notNullable()

073811c807223f65f85ff368fa2d35c58d3f568d
Gonzalo Nardini <nardinigonzalom@gmail.com>
2021-11-08 18:49:21 -0300

    Add endpoint to fetch user balances

--- src/blockscout.ts
+++ src/blockscout.ts
@@ -12,7 +12,7 @@ import {
   Faucet,
   TokenReceived,
   TokenSent,
-  Verification,
+  Verification
 } from './events'
 import { EscrowContractCall } from './events/EscrowContractCall'
 import { ExchangeContractCall } from './events/ExchangeContractCall'
@@ -55,7 +55,7 @@ export class BlockscoutAPI extends RESTDataSource {
 
   constructor() {
     super()
-    this.baseURL = BLOCKSCOUT_API
+    this.baseURL = `${BLOCKSCOUT_API}/graphql`
   }
 
   async getRawTokenTransactions(address: string): Promise<Transaction[]> {

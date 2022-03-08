0e6d3caadfb2a34fbb3875491d674374d1108d6a
Diego Vazquez <diego.vazquez@valoraapp.com>
2022-03-08 17:13:27 -0300

    Style changes

--- test/blockscoutV2.test.ts
+++ test/blockscoutV2.test.ts
@@ -87,7 +87,10 @@ jest.mock('../src/helpers/TokenInfoCache.ts', () => ({
       },
     }[symbol]
   },
-  getTokensAddresses: () => [TEST_GOLD_ADDRESS, TEST_DOLLAR_ADDRESS].map(address => address.toLowerCase())
+  getTokensAddresses: () =>
+    [TEST_GOLD_ADDRESS, TEST_DOLLAR_ADDRESS].map((address) =>
+      address.toLowerCase(),
+    ),
 }))
 
 // @ts-ignore

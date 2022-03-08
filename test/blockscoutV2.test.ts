5dc218593836efa64f204d43a8dcf57b6746a647
Diego Vazquez <diego.vazquez@valoraapp.com>
2022-03-08 17:13:07 -0300

    Filtering unknown tokens transactions

--- test/blockscoutV2.test.ts
+++ test/blockscoutV2.test.ts
@@ -87,6 +87,7 @@ jest.mock('../src/helpers/TokenInfoCache.ts', () => ({
       },
     }[symbol]
   },
+  getTokensAddresses: () => [TEST_GOLD_ADDRESS, TEST_DOLLAR_ADDRESS].map(address => address.toLowerCase())
 }))
 
 // @ts-ignore
